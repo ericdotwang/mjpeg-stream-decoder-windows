b2fce55c4edb364569b90ffbb593eaf78f6e2475
Eric <273578147@qq.com>
2015-12-29 18:24:19 +0800

    first commit

---
+++ MJPEG/MjpegProcessorTestWinForms/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace MjpegProcessorTestWinForms
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.btnStart = new System.Windows.Forms.Button();
+            this.image = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.AutoSize = true;
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnStart.Location = new System.Drawing.Point(0, 435);
+            this.btnStart.MinimumSize = new System.Drawing.Size(0, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(641, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // image
+            // 
+            this.image.BackColor = System.Drawing.Color.Black;
+            this.image.Dock = System.Windows.Forms.DockStyle.Top;
+            this.image.Location = new System.Drawing.Point(0, 0);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(641, 443);
+            this.image.TabIndex = 1;
+            this.image.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 475);
+            this.Controls.Add(this.image);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.PictureBox image;
+	}
+}
+
---
+++ MJPEG/MjpegProcessorTestWinForms/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using MjpegStreamProcessor;
+
+namespace MjpegProcessorTestWinForms
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+            this.image.SizeMode = PictureBoxSizeMode.Zoom;
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+            MjpegStreamDecoder mjpeg = new MjpegStreamDecoder();
+			mjpeg.FrameReady += mjpeg_FrameReady;
+			mjpeg.Error += mjpeg_Error;
+            mjpeg.ParseStream(new Uri("http://10.1.16.48:10086/?action=stream"));
+		}
+
+		private void mjpeg_FrameReady(object sender, FrameReadyEventArgs e)
+		{
+			image.Image = e.Bitmap;
+		}
+
+		void mjpeg_Error(object sender, ErrorEventArgs e)
+		{
+			MessageBox.Show(e.Message);
+		}
+	}
+}
